3088a7a38f84b06553e19c40f8c4147f032537ec
Mohamad <mohamadacma@gmail.com>
2026-01-06 23:49:29 -0600

    Add controller

--- src/main/java/com/marginalia/marginalia/controller/HealthController.java
+++ src/main/java/com/marginalia/marginalia/controller/HealthController.java
@@ -1,4 +1,47 @@
 package com.marginalia.marginalia.controller;
 
+import com.marginalia.marginalia.model.User;
+import com.marginalia.marginalia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class HealthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // GET all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // GET user by id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // POST create new user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // DELETE user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
